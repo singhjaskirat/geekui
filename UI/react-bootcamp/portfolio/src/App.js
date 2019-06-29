2530d2088ab07843412d5e75a2693f17a06d9e40
JaskiratSingh <jaskirat.18@gmail.com>
2019-06-29 15:19:51 +1000

    updates

--- UI/react-bootcamp/portfolio/src/App.js
+++ UI/react-bootcamp/portfolio/src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//creating a componenet
+class App extends Component {
+    render(){
+        return(
+            <div>
+                <p>Hello !</p>
+                <p>How are u doing?</p>
+            </div>
+        )
+    }
 }
 
+//element is a smallest class
+//component can be a bigger one
+
 export default App;
+//exporting the component
